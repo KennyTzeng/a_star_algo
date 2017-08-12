67745c19667b3776fb0729e7c536eafd77566e6e
KennyTseng <kenny29282000@gmail.com>
2017-08-12 18:43:57 +0800

    add calculate_hn(), fix get_lowest_fn()

--- a_star_algo.py
+++ a_star_algo.py
@@ -4,6 +4,8 @@ import numpy as np
 map_width = 0
 map_height = 0
 mapList = []
+S = None
+E = None
 
 class Node:
 
@@ -24,10 +26,14 @@ def calculate_gn(node):
 	else:
 		return calculate_gn(node.parent) + distance_to_parent(node)
 
-def get_lowest_fn(list, E):
+def calculate_hn(node):
+	global E
+	return math.sqrt((node.x-E.x)**2 + (node.y-E.y)**2)
+
+def get_lowest_fn(list):
 	lowest_fn = 10000000
 	for i, node in enumerate(list):
-		fn = calculate_gn(node) + math.sqrt((node.x-E.x)**2 + (node.y-E.y)**2)
+		fn = calculate_gn(node) + calculate_hn(node)
 		if(fn < lowest_fn):
 			lowest_fn = fn
 			lowest_index = i
@@ -56,6 +62,41 @@ def canMove(node):
 
 def main():
 	# load the map
+	global mapList, S, E
+	lineList = []
+	
+	with open('map.txt', 'r', encoding='utf-8') as map:
+		for i, line in enumerate(map):
+			for j, char in enumerate(line):
+				if(char == '\n'):
+					continue
+				if(char == 'S'):
+					S = Node(None, i, j)
+				if(char == 'E'):
+					E = Node(None, i, j)
+				lineList.append(char)
+			mapList.append(lineList.copy())
+			lineList.clear()
+
+	global map_width, map_height
+	map_width = len(mapList[0])
+	map_height = len(mapList)
+
+	openList = []
+	closedList = []
+	openList.append(S)
+	while(len(openList) != 0):
+		lowest_index = get_lowest_fn(openList)
+		n = openList[lowest_index]
+		if(n.x == E.x and n.y == E.y):
+			return n
+		closedList.append(openList.pop(lowest_index))
+		for (nTemp, moveCost) in canMove(n):
+			gnTemp = calculate_gn(n) + moveCost
+
+
+def test():
+	# load the map
 	global mapList
 	lineList = []
 	
@@ -74,34 +115,9 @@ def main():
 	global map_width, map_height
 	map_width = len(mapList[0])
 	map_height = len(mapList)
-
-	openList = []
-	closedList = []
-	openList.append(S)
-	while(len(openList) != 0):
-		lowest_index = get_lowest_fn(openList)
-		n = openList[lowest_index]
-		if(n.x == E.x and n.y == E.y):
-			return n
-		closedList.append(openList.pop(lowest_index))
-
-
-def test():
-	a = Node(None, 1, 1)
-	b = Node(a, 2, 1)
-	c = Node(b, 2, 2)
-	d = Node(None, 1, 1)
-	l = []
-	l2 = []
-	l.append(a)
-	l.append(b)
-	l.append(c)
-	print(l)
-	l.remove(a)
-	print(l)
-	l2.append(l.pop(1))
-	print(l)
-	print(l2)
+	n = Node(None,1,2)
+	list = canMove(n)
+	print(list)
 
 
 if(__name__ == '__main__'):
